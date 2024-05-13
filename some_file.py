c132e40b8dab5a15aca3f9eebd863f71a8c08840
Vanya Iksanov <vanyathedonkey@yandex.ru>
2024-05-13 14:50:58 +0300

    new programm

--- some_file.py
+++ some_file.py
@@ -1,3 +1,15 @@
-print('My first repository!')
+# print('My first repository!')
 
-print('New commit for test')
+# print('New commit for test')
+import requests
+import pprint
+
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+
+response = requests.get(api_url)
+
+if response.status_code == 200:
+    pprint.pprint(response.text)
+else:
+    pprint.pprint(response.status_code)
